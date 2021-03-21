da5ca857c307ba406b68bb29e24927eb8990668b
Roberto de Campos <roberto.rfc@live.com>
2021-03-21 16:09:51 -0300

    ajustando pagina inicial

--- index.php
+++ index.php
@@ -1,16 +1,39 @@
 <?php
     require_once 'menu.php';
 ?>
-    <div class="row">
-      <div class="col-lg-12 text-center">
-        <h1 class="mt-5">A Bootstrap 4 Starter Template</h1>
-        <p class="lead">Complete with pre-defined file paths and responsive navigation!</p>
-        <ul class="list-unstyled">
-          <li>Bootstrap 4.5.3</li>
-          <li>jQuery 3.5.1</li>
-        </ul>
-      </div>
+
+<div class="row">
+    <div class="col-md-6 text-center">
     </div>
+    <div class="col-md-6 text-center">
+        <img src="img/bolo.jpg" alt="" width="300px" height="300px">
+    </div>
+</div>
+<br>
+<div class="row">
+    <div class="col-md-6 text-center">
+        <img src="img/cupcake.jpg" alt="" width="300px" height="300px">
+    </div>
+    <div class="col-md-6 text-center">
+    </div>
+</div>
+<br>
+<div class="row">
+    <div class="col-md-6 text-center">
+    </div>
+    <div class="col-md-6 text-center">
+        <img src="img/naked cake.jpg" alt="" width="300px" height="300px">
+    </div>
+</div>
+<br>
+<div class="row">
+    <div class="col-md-6 text-center">
+        <img src="img/taca.jpg" alt="" width="300px" height="300px">
+    </div>
+    <div class="col-md-6 text-center">
+    </div>
+</div>
+
 <?php
     require_once 'rodape.php';
 ?>
--- menu.php
+++ menu.php
@@ -1,52 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-<head>
+        <title>Bare - Start Bootstrap Template</title>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/site.css" rel="stylesheet">
+    </head>
 
-  <title>Bare - Start Bootstrap Template</title>
+    <body>
 
-  <!-- Bootstrap core CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/site.css" rel="stylesheet">
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg static-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="img/logo50.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
-</head>
+        <br><br>
 
-<body>
-
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg static-top">
-    <div class="container">
-      <a class="navbar-brand" href="#">
-          <img src="img/logo50.png" alt="">
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-  <!-- Page Content -->
-  <div class="container">
+        <!-- Page Content -->
+        <div class="container body">
