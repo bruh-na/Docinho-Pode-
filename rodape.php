5d97123be96decea3f49e747f10f33d5ee963890
Roberto de Campos <roberto.rfc@live.com>
2021-03-21 15:40:15 -0300

    ajustando o rodape

--- rodape.php
+++ rodape.php
@@ -1,8 +1,40 @@
 </div>
 
+<footer>
+    <div class="row my-5 justify-content-center py-5">
+        <div class="col-11">
+            <div class="row ">
+                <div class="col-xl-8 col-md-4 col-sm-4 col-12 my-auto mx-auto a">
+                    <img src="img/logo200.png" alt="">
+                </div>
+                <div class="col-xl-2 col-md-4 col-sm-4 col-12">
+                    <h6 class="mb-3 mb-lg-4 bold-text "><b>MENU</b></h6>
+                    <ul class="list-unstyled">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Blog</li>
+                        <li>Portfolio</li>
+                    </ul>
+                </div>
+                <div class="col-xl-2 col-md-4 col-sm-4 col-12">
+                    <h6 class="mb-3 mb-lg-4 bold-text mt-sm-0 mt-5"><b>ENDEREÇO</b></h6>
+                    <p class="mb-1">RUA SANHAÇO, 313</p>
+                    <p>CAIEIRAS / SP</p>
+                </div>
+            </div>
+            <div class="row ">
+                <div class="col-xl-8 col-md-4 col-sm-4 col-auto my-md-0 mt-5 order-sm-1 order-3 align-self-end">
+                    <small><span>&#174;</span> All Rights Reserved.</small>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
 <!-- Bootstrap core JavaScript -->
 <script src="js/jquery-3.6.0.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></script>
 
 </body>
 
