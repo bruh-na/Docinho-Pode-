9f0f702e16b94e1216991a55da64dff28aa2d6ea
Silvana <sil.scoutinho@gmail.com>
2021-03-25 21:50:38 -0300

    Adicionando links para as redes sociais

--- rodape.php
+++ rodape.php
@@ -3,7 +3,7 @@
 <footer>
     <div class="row my-5 justify-content-center py-5">
         <div class="col-11">
-            <div class="row ">
+            <div class="row">
                 <div class="col-xl-8 col-md-4 col-sm-4 col-12 my-auto mx-auto a">
                     <img src="img/logo200.png" alt="">
                 </div>
@@ -20,9 +20,12 @@
                     <h6 class="mb-3 mb-lg-4 bold-text mt-sm-0 mt-5"><b>ENDEREÇO</b></h6>
                     <p class="mb-1">RUA SANHAÇO, 313</p>
                     <p>CAIEIRAS / SP</p>
+                    <a href="https://fb.me/docinhopode" target="alt" title="Facebook"><img src="img/icons/facebook.png" /></a>
+                    <a href="https://instagram.com/docinhopode" target="alt" title="Instagram"><img src="img/icons/instagram.png" /></a>
+                    <a href="https://wa.me/5511977975167" target="alt" title="WhatsApp"><img src="img/icons/whatsapp.png" /></a>
                 </div>
             </div>
-            <div class="row ">
+            <div class="row">
                 <div class="col-xl-8 col-md-4 col-sm-4 col-auto my-md-0 mt-5 order-sm-1 order-3 align-self-end">
                     <small><span>&#174;</span> All Rights Reserved.</small>
                 </div>
