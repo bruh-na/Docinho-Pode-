60777a3e4ac9717b5286f86cacafe2a7fa39dfce
Silvana <sil.scoutinho@gmail.com>
2021-03-25 21:50:13 -0300

    Alteração do titulo do site

--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>Bare - Start Bootstrap Template</title>
+        <title>Docinho? Pode!</title>
 
         <!-- Bootstrap core CSS -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
